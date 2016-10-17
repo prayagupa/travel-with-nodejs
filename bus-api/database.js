658babc5722e025f815dc227ae863aab4ba0370d
Prayag Upd <prayag.upd@gmail.com>
2016-10-17 17:40:25 -0500

    add deciphering a message

--- bus-api/database.js
+++ bus-api/database.js
@@ -8,22 +8,28 @@ var Db = require('mongodb').Db
 var mongoClient = new MongoClient(new Server("localhost", 27017))
 
 module.exports = {
-   insert : function insert(data) {
-             filesystem.appendFile("/var/log/atom", data, function(err) {
-                 if(err) {
-                    return console.log(err);
-                 }
-                 console.log("The data is saved to Database!");
-           })
- 
-   },
-  
-   insert_data : function insert_data(document) {
-                MongoClient.connect("mongodb://172.17.0.1:27017/easyride", function(err, db){
-                  assert.equal(null, err)
-                  db.collection("Feedback").insertOne(document)
-                  console.log(db.collection("Feedback").findOne())
-                  db.close()
-                })
-   }
+    insert: function insert(data) {
+        filesystem.appendFile("/var/log/atom", data, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The data is saved to Database!");
+        })
+
+    },
+    insert_data: function insert_data(document) {
+        MongoClient.connect("mongodb://172.17.0.1:27017/easyride", function (err, db) {
+            assert.equal(null, err)
+            db.collection("Feedback").insertOne(document)
+            console.log(db.collection("Feedback").findOne())
+            db.close()
+        })
+    },
+    insertMessage: function insert_data(document) {
+        MongoClient.connect("mongodb://localhost:27017/easyride", function (err, db) {
+            assert.equal(null, err)
+            db.collection("Messages").insertOne(document)
+            db.close()
+        })
+    }
 }
--- bus-api/server.js
+++ bus-api/server.js
@@ -5,6 +5,9 @@ var Formidable = require("formidable");
 var https = require('https');
 var Database = require('./database')
 var Util = require('./util')
+var MongoClient = require('mongodb').MongoClient
+const crypto = require('crypto');
+const decipher = crypto.createDecipher('aes256', 'asaadsaad');
 
 app.set('json spaces', 40)
 app.set('view engine', 'ejs');
@@ -18,11 +21,33 @@ app.get('/buses', (req, res) => {
     res.render('buses', {"buses": syncJson})
 });
 
-app.get('/image', (req, res) => {
-    Util.download('https://lastfm-img2.akamaized.net/i/u/64s/15cc734fb0e045e3baac02674d2092d6.png', 'porcupine.png', ()=> {
-        console.log("downloaded to porcupine.png")
-        res.sendFile(__dirname + '/porcupine.png');
+
+app.get('/message', (req, res) => {
+    res.render('bus_message')
+});
+
+app.post('/message', (req, res) => {
+    var message = new Formidable.IncomingForm()
+    message.parse(req, (err, fields, files) => {
+        Database.insertMessage(fields)
+        MongoClient.connect("mongodb://localhost:27017/easyride", function (err, db) {
+            db.collection("Messages").findOne({}, (error, data) => {
+                var decrypted = decipher.update(data.message, 'hex', 'utf8');
+                decrypted += decipher.final('utf8');
+                console.log(decrypted);
+
+                res.render("bus_message", {message: decrypted})
+            })
+        })
     })
+});
+
+app.get('/image', (req, res) => {
+    Util.download('https://lastfm-img2.akamaized.net/i/u/64s/15cc734fb0e045e3baac02674d2092d6.png', 'porcupine.png',
+        ()=> {
+            console.log("downloaded to porcupine.png")
+            res.sendFile(__dirname + '/porcupine.png');
+        })
 })
 
 app.get('/contactus', (req, res) => {
