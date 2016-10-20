7fa3df1f5ba0fb20a2e9695d6bcfe93804fd08ab
Prayag Upd <prayag.upd@gmail.com>
2016-10-20 12:03:28 -0500

    add products li view, with details

---
+++ marketplace-view-angular/app/mock-products.ts
@@ -0,0 +1,14 @@
+import { Product } from './product'
+
+export const PRODUCTS: Product[] = [
+  { id: 11, name: 'HCE' },
+  { id: 12, name: 'sleepmakeswaves' },
+  { id: 13, name: 'Metallica Reload 5' },
+  { id: 14, name: 'Metallica Reload 5s' },
+  { id: 15, name: 'Metallica Reload 5c' },
+  { id: 16, name: 'Metallica Reload 6' },
+  { id: 17, name: 'Metallica Reload 6s' },
+  { id: 18, name: 'Metallica Reload 6s plus' },
+  { id: 19, name: 'Metallica Reload 7' },
+  { id: 20, name: 'Slayer album' }
+];
