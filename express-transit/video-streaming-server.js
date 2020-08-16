e811cc138b8efd2b316e604cb22f72dd42a2abb0
Upadhyay <prayag.upd@gmail.com>
2020-08-15 20:13:09 -0700

    fix path

--- express-transit/video-streaming-server.js
+++ express-transit/video-streaming-server.js
@@ -4,24 +4,25 @@ const path = require('path')
 const app = express()
  
 app.get('/', function(req, res) {
-res.sendFile(path.join(__dirname + '/index.html'))
+	res.sendFile(path.join(__dirname + '/index.html'))
 })
  
 app.get('/video', function(req, res) {
-	console.log("video request");
-	const vodeo_path = 'assets/ad.mp4'
+	console.log("GET /video request");
+	const video_path = path.join(__dirname + '/assets/ad.mp4')
+
 	const stat = fs.statSync(video_path)
 	const fileSize = stat.size
 	const range = req.headers.range
 
-	console.log(stat);
+	console.log(video_path + "\n" + JSON.stringify(stat));
 	if (range) {
 		const parts = range.replace(/bytes=/, "").split("-")
 		const start = parseInt(parts[0], 10)
 		const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1
  
 		const chunksize = (end-start)+1
-		const file = fs.createReadStream(path, {start, end})
+		const file = fs.createReadStream(video_path, {start, end})
 		const head = {
 			'Content-Range': `bytes ${start}-${end}/${fileSize}`,
 			'Accept-Ranges': 'bytes',
@@ -32,11 +33,11 @@ app.get('/video', function(req, res) {
 		res.writeHead(206, head)
 		file.pipe(res)
 	} else {
-		const head = {
+		const headers = {
 			'Content-Length': fileSize,
 			'Content-Type': 'video/mp4',
 		}
-		res.writeHead(200, head)
+		res.writeHead(200, headers)
 		fs.createReadStream(video_path).pipe(res)
 	}
 })
